03a238dfb34c37344c6e9acb367d5d6c945b4e36
CoccoRandela <rocco.candela94@gmail.com>
2022-04-08 11:28:14 +0100

    Make navbar and dropdown menus

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-const columnNumber = prompt('how many squares x row', '');
+/* grid creator */
 const gridContainer = document.querySelector('.grid-container');
-gridContainer.style.gridTemplateColumns = `repeat(${columnNumber}, 1fr)`;
-for (let i=1; i<=(columnNumber*columnNumber); i++) {
+gridContainer.style.gridTemplateColumns = `repeat(16, 1fr)`;
+for (let i=1; i<=(16*16); i++) {
     let square = document.createElement('div');
     square.classList.add('square');
     gridContainer.appendChild(square);
 }
 
+/* squares color */
 const squares = document.querySelectorAll('.square');
 squares.forEach(square => {
     square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'black';
-        
+        square.style.backgroundColor = 'transparent';
+        square.style.border = '.1rem solid red'; 
     })
+})
+
+/* dropdown menu */
+document.addEventListener('click', e => {
+    const dropdownButtons = document.querySelectorAll('.dropdown-button');
+    dropdownButtons.forEach(dropdownButton => {
+        if (e.target != dropdownButton && e.target.closest('.dropdown-section')) return;
+        let dropdownMenu = dropdownButton.nextElementSibling;
+        if (e.target == dropdownButton) {
+            dropdownMenu.classList.toggle('selected');
+        }
+        if (e.target != dropdownButton && dropdownMenu.classList.contains('selected')) {
+            dropdownMenu.classList.remove('selected');
+        }
+    })
+
 })
